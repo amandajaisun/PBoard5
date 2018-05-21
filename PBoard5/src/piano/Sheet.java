3aa5bd52bb6acc24d11fb3da5086f8b49c3fa9bf
amandasun <amandasun@192.168.7.25>
2018-05-20 23:23:06 -0700

    finished Sheet

--- PBoard5/src/piano/Sheet.java
+++ PBoard5/src/piano/Sheet.java
@@ -4,12 +4,12 @@ import java.awt.Graphics;
 
 public class Sheet {
 	int x,y,width,height,margin,speed;
-	public Sheet(int x, int y, int width, int height, int margin){
+	public Sheet(int x, int y, int width, int height){
 		this.x = x;
 		this.y = y;
 		this.width = width;
 		this.height = height;
-		this.margin = margin;
+		margin = height/4;
 	}
 	
 	public void translate(int speed) {
@@ -17,6 +17,9 @@ public class Sheet {
 	}
 	public void draw(Graphics g) {
 		g.drawRect(x, y, width, height);
+		g.drawLine(x, y+margin, x+width, y+margin);
+		g.drawLine(x, y+2*margin, x+width, y+2*margin);
+		g.drawLine(x, y+3*margin, x+width, y+3*margin);
 		
 	}
-}
+} 
