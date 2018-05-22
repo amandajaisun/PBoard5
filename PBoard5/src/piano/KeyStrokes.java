a77e3faaff925e229b3a1a5f4756108af304c965
rithika <rithika@192.168.1.7>
2018-05-21 20:08:25 -0700

    replays recording faster than before

--- PBoard5/src/piano/KeyStrokes.java
+++ PBoard5/src/piano/KeyStrokes.java
@@ -10,48 +10,59 @@ public class KeyStrokes {
 	private ArrayList<Key> keys;
 	private long startTime;
 	private Sound sound;
-	
-	
+
 	public KeyStrokes() {
 		timestamps = new ArrayList<Long>();
 		keys = new ArrayList<Key>();
 		startTime = System.currentTimeMillis();
-		 sound = new Sound("");
+		sound = new Sound("");
 	}
-	
+
 	public void addKeyStroke(Key k) {
-		keys.add(k);
+
+		// if (keys.size() == 0 ) {
 		timestamps.add(System.currentTimeMillis() - startTime);
+		// } else {
+		// timestamps.add(System.currentTimeMillis() - startTime +
+		// timestamps.get(keys.size()-1));
+		// }
+
+		keys.add(k);
 	}
-	
+
 	public void playBack() {
-		
-		int i = 0;
-		while(i < timestamps.size()) {
-			try {
-				Key k = keys.get(i);
-				String mp3 = k.getPianoLetter() + ".mp3"; 
-				Thread.sleep(timestamps.get(i));
-				 try {
+		if (keys != null) {
+			int i = 0;
+			while (i < timestamps.size()) {
+				try {
+					Key k = keys.get(i);
+					String mp3 = k.getPianoLetter() + ".mp3";
+					if (i == 0) {
+						Thread.sleep(timestamps.get(i));
+					} else {
+						Thread.sleep(timestamps.get(i) - timestamps.get(i - 1));
+					}
+					try {
 						sound.playSound(mp3);
 					} catch (JavaLayerException e1) {
 						// TODO Auto-generated catch block
 						e1.printStackTrace();
 					}
-			} catch (InterruptedException e) {
-				e.printStackTrace();
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+				i++;
+
 			}
-			i++;
-			
 		}
 	}
-	
+
 	public String toString() {
 		String output = "";
-		for (int i = 0; i< timestamps.size(); i++) {
-			output += ( "(" + keys.get(i).getPianoLetter() + ", " + timestamps.get(i) + ")\n");
+		for (int i = 0; i < timestamps.size(); i++) {
+			output += ("(" + keys.get(i).getPianoLetter() + ", " + timestamps.get(i) + ")\n");
 		}
 		return output;
-		
+
 	}
 }
